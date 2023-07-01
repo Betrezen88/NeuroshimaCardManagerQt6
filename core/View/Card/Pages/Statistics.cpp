86e8034a772ee978418fb45cfce870c1eecbaa32
Betrezen88 <wyszyn2000@gmail.com>
2023-07-01 21:18:42 +0200

    Added Statistics class.

---
+++ core/View/Card/Pages/Statistics.cpp
@@ -0,0 +1,255 @@
+#include "Statistics.h"
+
+Statistics::Statistics(QObject *parent)
+    : Page(parent)
+    , m_type(Types::Page::Statistics)
+{
+
+}
+
+Statistics::Statistics(const StatisticsData &data, QObject *parent)
+    : Page(parent)
+    , m_type(Types::Page::Statistics)
+    , m_name(data.name)
+    , m_surname(data.surname)
+    , m_nickanem(data.nickname)
+    , m_disease(new Disease(data.disease, this))
+    , m_experience(new Experience(data.experience, this))
+    , m_origin(new Origin(data.origin, this))
+    , m_profession(new Profession(data.profession, this))
+    , m_specialization(new Specialization(data.specialization, this))
+    , m_reputation(new Reputation(data.places, this))
+{
+    for (const auto &attribute: data.attributes) {
+        m_attributes.append(new Attribute(attribute, this));
+    }
+
+    for (const auto &otherSkill: data.otherSkills) {
+        m_otherSkills.append(new OtherSkill(otherSkill, this));
+    }
+
+    for (const auto &trick: data.tricks) {
+        m_tricks.append(new Trick(trick, this));
+    }
+}
+
+QString Statistics::fullname() const
+{
+    return m_name + (m_nickanem.isEmpty() ? " " : " '"+m_nickanem+"' ") + m_surname;
+}
+
+Types::Page Statistics::type() const
+{
+    return m_type;
+}
+
+Disease *Statistics::disease() const
+{
+    return m_disease;
+}
+
+Experience *Statistics::experience() const
+{
+    return m_experience;
+}
+
+Origin *Statistics::origin() const
+{
+    return m_origin;
+}
+
+Profession *Statistics::profession() const
+{
+    return m_profession;
+}
+
+Specialization *Statistics::specialization() const
+{
+    return m_specialization;
+}
+
+QQmlListProperty<Attribute> Statistics::attributes()
+{
+    return QQmlListProperty<Attribute>(this, this,
+                                       &Statistics::attributesCount,
+                                       &Statistics::attribute);
+}
+
+qsizetype Statistics::attributesCount() const
+{
+    return m_attributes.count();
+}
+
+Attribute *Statistics::attribute(qsizetype index)
+{
+    return m_attributes.at(index);
+}
+
+Attribute *Statistics::attribute(const QString &name)
+{
+    auto found = std::find_if(m_attributes.constBegin(), m_attributes.constEnd(), [&name](const Attribute* attribute){
+        return attribute->name() == name;
+    });
+
+    if ( found != m_attributes.cend() ) {
+        return *found;
+    }
+    return nullptr;
+}
+
+qsizetype Statistics::attributesCount(QQmlListProperty<Attribute> *list)
+{
+    return reinterpret_cast<Statistics*>(list->data)->attributesCount();
+}
+
+Attribute *Statistics::attribute(QQmlListProperty<Attribute> *list, qsizetype index)
+{
+    return reinterpret_cast<Statistics*>(list->data)->attribute(index);
+}
+
+QQmlListProperty<OtherSkill> Statistics::otherSkills()
+{
+    return QQmlListProperty<OtherSkill>(this, this,
+                                        &Statistics::otherSkillsCount,
+                                        &Statistics::otherSkill);
+}
+
+qsizetype Statistics::otherSkillsCount() const
+{
+    return m_otherSkills.count();
+}
+
+OtherSkill *Statistics::otherSkill(qsizetype index)
+{
+    return m_otherSkills.at(index);
+}
+
+qsizetype Statistics::otherSkillsCount(QQmlListProperty<OtherSkill> *list)
+{
+    return reinterpret_cast<Statistics*>(list->data)->otherSkillsCount();
+}
+
+OtherSkill *Statistics::otherSkill(QQmlListProperty<OtherSkill> *list, qsizetype index)
+{
+    return reinterpret_cast<Statistics*>(list->data)->otherSkill(index);
+}
+
+qsizetype Statistics::tricksCount(QQmlListProperty<Trick> *list)
+{
+    return reinterpret_cast<Statistics*>(list->data)->tricksCount();
+}
+
+Trick *Statistics::trick(QQmlListProperty<Trick> *list, qsizetype index)
+{
+    return reinterpret_cast<Statistics*>(list->data)->trick(index);
+}
+
+void Statistics::addWound(QQmlListProperty<Wound> *list, Wound *wound)
+{
+    return reinterpret_cast<Statistics*>(list->data)->addWound(wound);
+}
+
+qsizetype Statistics::woundsCount(QQmlListProperty<Wound> *list)
+{
+    return reinterpret_cast<Statistics*>(list->data)->woundsCount();
+}
+
+Wound *Statistics::wound(QQmlListProperty<Wound> *list, qsizetype index)
+{
+    return reinterpret_cast<Statistics*>(list->data)->wound(index);
+}
+
+void Statistics::clearWounds(QQmlListProperty<Wound> *list)
+{
+    return reinterpret_cast<Statistics*>(list->data)->clearWounds();
+}
+
+Reputation *Statistics::reputation() const
+{
+    return m_reputation;
+}
+
+QQmlListProperty<Trick> Statistics::tricks()
+{
+    return QQmlListProperty<Trick>(this, this,
+                                   &Statistics::tricksCount,
+                                   &Statistics::trick);
+}
+
+qsizetype Statistics::tricksCount() const
+{
+    return m_tricks.count();
+}
+
+Trick *Statistics::trick(qsizetype index)
+{
+    return m_tricks.at(index);
+}
+
+QQmlListProperty<Wound> Statistics::wounds()
+{
+    return QQmlListProperty<Wound>(this, this,
+                                   &Statistics::addWound,
+                                   &Statistics::woundsCount,
+                                   &Statistics::wound,
+                                   &Statistics::clearWounds);
+}
+
+void Statistics::addWound(Wound *wound)
+{
+    m_wounds.append(wound);
+}
+
+qsizetype Statistics::woundsCount() const
+{
+    return m_wounds.count();
+}
+
+Wound *Statistics::wound(qsizetype index)
+{
+    return m_wounds.at(index);
+}
+
+void Statistics::clearWounds()
+{
+    m_wounds.clear();
+}
+
+StatisticsData Statistics::data()
+{
+    QVector<AttributeData> attributes;
+    for ( const auto& attribute: m_attributes ) {
+        attributes.append(attribute->data());
+    }
+
+    QVector<OtherSkillData> otherSkills;
+    for ( const auto& otherSkill: m_otherSkills ) {
+        otherSkills.append( otherSkill->data() );
+    }
+
+    QVector<TrickData> tricks;
+    for ( const auto& trick: m_tricks ) {
+        tricks.append( trick->data() );
+    }
+
+    QVector<WoundData> wounds;
+    for ( const auto& wound: m_wounds ) {
+        wounds.append( wound->data() );
+    }
+
+    return StatisticsData{
+        .name = m_name,
+        .surname = m_surname,
+        .nickname = m_nickanem,
+        .origin = m_origin->data(),
+        .profession = m_profession->data(),
+        .specialization = m_specialization->data(),
+        .disease = m_disease->data(),
+        .attributes = attributes,
+        .otherSkills = otherSkills,
+        .tricks = tricks,
+        .experience = m_experience->data(),
+        .wounds = wounds,
+        .places = m_reputation->data()
+    };
+}
---
+++ core/View/Card/Pages/Statistics.h
@@ -0,0 +1,106 @@
+#ifndef STATISTICS_H
+#define STATISTICS_H
+
+#include <QQmlListProperty>
+#include <QtQml/qqmlregistration.h>
+
+#include "Page.h"
+
+#include "../Statistics/Attribute.h"
+#include "../Statistics/Disease.h"
+#include "../Statistics/Experience.h"
+#include "../Statistics/Origin.h"
+#include "../Statistics/Otherskill.h"
+#include "../Statistics/Profession.h"
+#include "../Statistics/Reputation.h"
+#include "../Statistics/Specialization.h"
+#include "../Statistics/Trick.h"
+#include "../Statistics/Wound.h"
+
+#include "../../../Data/Pages/StatisticsData.h"
+
+class Statistics : public Page
+{
+    Q_OBJECT
+    QML_ELEMENT
+    Q_PROPERTY(QString fullname READ fullname CONSTANT)
+    Q_PROPERTY(QQmlListProperty<Attribute> attributes READ attributes CONSTANT)
+    Q_PROPERTY(Disease* disease READ disease CONSTANT)
+    Q_PROPERTY(Experience* experience READ experience CONSTANT)
+    Q_PROPERTY(Origin* origin READ origin CONSTANT)
+    Q_PROPERTY(QQmlListProperty<OtherSkill> otherSkills READ otherSkills CONSTANT)
+    Q_PROPERTY(Profession* profession READ profession CONSTANT)
+    Q_PROPERTY(Specialization* specialization READ specialization CONSTANT)
+    Q_PROPERTY(QQmlListProperty<Trick> tricks READ tricks CONSTANT)
+    Q_PROPERTY(Reputation* reputation READ reputation CONSTANT)
+    Q_PROPERTY(QQmlListProperty<Wound> wounds READ wounds NOTIFY woundsChanged)
+
+public:
+    explicit Statistics(QObject *parent = nullptr);
+    explicit Statistics(const StatisticsData &data, QObject *parent = nullptr);
+
+    QString fullname() const;
+
+    Types::Page type() const;
+    Disease *disease() const;
+    Experience *experience() const;
+    Origin *origin() const;
+    Profession *profession() const;
+    Specialization *specialization() const;
+
+    QQmlListProperty<Attribute> attributes();
+    qsizetype attributesCount() const;
+    Attribute* attribute(qsizetype index);
+    Q_INVOKABLE Attribute* attribute(const QString& name);
+
+    QQmlListProperty<OtherSkill> otherSkills();
+    qsizetype otherSkillsCount() const;
+    OtherSkill* otherSkill(qsizetype index);
+
+    Reputation *reputation() const;
+
+    QQmlListProperty<Trick> tricks();
+    qsizetype tricksCount() const;
+    Trick* trick(qsizetype index);
+
+    QQmlListProperty<Wound> wounds();
+    void addWound(Wound* wound);
+    qsizetype woundsCount() const;
+    Wound* wound(qsizetype index);
+    void clearWounds();
+
+    StatisticsData data();
+
+signals:
+    void woundsChanged();
+
+private:
+    static qsizetype attributesCount(QQmlListProperty<Attribute> *list);
+    static Attribute* attribute(QQmlListProperty<Attribute> *list, qsizetype index);
+    static qsizetype otherSkillsCount(QQmlListProperty<OtherSkill> *list);
+    static OtherSkill* otherSkill(QQmlListProperty<OtherSkill> *list, qsizetype index);
+    static qsizetype tricksCount(QQmlListProperty<Trick> *list);
+    static Trick* trick(QQmlListProperty<Trick> *list, qsizetype index);
+    static void addWound(QQmlListProperty<Wound> *list, Wound* wound);
+    static qsizetype woundsCount(QQmlListProperty<Wound> *list);
+    static Wound* wound(QQmlListProperty<Wound> *list, qsizetype index);
+    static void clearWounds(QQmlListProperty<Wound> *list);
+
+private:
+    Types::Page m_type;
+    QString m_name;
+    QString m_surname;
+    QString m_nickanem;
+    Disease* m_disease{nullptr};
+    Experience* m_experience{nullptr};
+    Origin* m_origin{nullptr};
+    Profession* m_profession{nullptr};
+    Specialization* m_specialization{nullptr};
+    QVector<Attribute*> m_attributes;
+    QVector<OtherSkill*> m_otherSkills;
+    QVector<Trick*> m_tricks;
+    Reputation* m_reputation{nullptr};
+    QVector<Wound*> m_wounds;
+};
+
+#endif // STATISTICS_H
