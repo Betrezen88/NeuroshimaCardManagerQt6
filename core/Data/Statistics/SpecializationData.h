55da40748bbfffdd49b364495caae28d2c4e8b90
Betrezen88 <wyszyn2000@gmail.com>
2023-07-01 18:59:13 +0200

    Added SpecializationData file.

---
+++ core/Data/Statistics/SpecializationData.h
@@ -0,0 +1,11 @@
+#ifndef SPECIALIZATIONDATA_H
+#define SPECIALIZATIONDATA_H
+
+#include <QString>
+
+struct SpecializationData {
+    QString name;
+    QString description;
+};
+
+#endif // SPECIALIZATIONDATA_H
