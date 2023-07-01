4439469b8c8099a2fd1bc4706f69262baa4ae3f7
Betrezen88 <wyszyn2000@gmail.com>
2023-07-01 18:56:34 +0200

    Added OriginData file.

---
+++ core/Data/Statistics/OriginData.h
@@ -0,0 +1,12 @@
+#ifndef ORIGINDATA_H
+#define ORIGINDATA_H
+
+#include "FeatureData.h"
+
+struct OriginData {
+    QString name;
+    QString description;
+    FeatureData feature;
+};
+
+#endif // ORIGINDATA_H
