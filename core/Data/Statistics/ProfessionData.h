dc3c791bebdcf9fd834dbf774681649d14f08576
Betrezen88 <wyszyn2000@gmail.com>
2023-07-01 18:56:59 +0200

    Added ProfessionData file.

---
+++ core/Data/Statistics/ProfessionData.h
@@ -0,0 +1,12 @@
+#ifndef PROFESSIONDATA_H
+#define PROFESSIONDATA_H
+
+#include "FeatureData.h"
+
+struct ProfessionData {
+    QString name;
+    QString description;
+    FeatureData feature;
+};
+
+#endif // PROFESSIONDATA_H
