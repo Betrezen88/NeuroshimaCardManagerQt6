1b353adbc95f4741604aeb2286cc4ff520e0e5d7
Betrezen88 <wyszyn2000@gmail.com>
2023-08-27 19:56:46 +0200

    Added AttributeBonusSource class.

---
+++ core/Source/Statistics/Bonus/AttributeBonusSource.h
@@ -0,0 +1,32 @@
+#ifndef ATTRIBUTEBONUSSOURCE_H
+#define ATTRIBUTEBONUSSOURCE_H
+
+#include <QObject>
+#include "qqmlintegration.h"
+#include "../../../core/Data/Common/Types.h"
+
+class AttributeBonusSource : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(int value READ value CONSTANT)
+    QML_ELEMENT
+
+public:
+    explicit AttributeBonusSource(QObject* parent = nullptr) : QObject{parent} {}
+    explicit AttributeBonusSource(const int value, QObject* parent = nullptr)
+        : QObject{parent}
+        , m_value(value)
+    {}
+    virtual ~AttributeBonusSource() {}
+
+    virtual Types::AttributeBonus type() const = 0;
+
+    inline int value() const {
+        return m_value;
+    }
+
+private:
+    int m_value{0};
+};
+
+#endif // ATTRIBUTEBONUSSOURCE_H
