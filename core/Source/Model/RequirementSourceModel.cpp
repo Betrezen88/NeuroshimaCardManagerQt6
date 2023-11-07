949c1a6f9f8313a58db5c4dad9c13f7956793801
Betrezen88 <wyszyn2000@gmail.com>
2023-11-07 15:26:59 +0100

    Added class RequirmentSourceModel.

---
+++ core/Source/Model/RequirementSourceModel.cpp
@@ -0,0 +1,40 @@
+#include "RequirementSourceModel.h"
+
+RequirementSourceModel::RequirementSourceModel(QObject *parent)
+    : QAbstractListModel{parent}
+{
+
+}
+
+int RequirementSourceModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+    return m_requirments.count();
+}
+
+QVariant RequirementSourceModel::data(const QModelIndex &index, int role) const
+{
+    if ( index.row() < 0 || index.row() >= m_requirments.count() )
+        return QVariant();
+
+    if ( role == RequirementRoles::NameRole )
+        return m_requirments.at(index.row())->name();
+    if ( role == RequirementRoles::TypeRole )
+        return static_cast<int>(m_requirments.at(index.row())->type());
+    if ( role == RequirementRoles::ValueRole )
+        return m_requirments.at(index.row())->value();
+    if ( role == RequirementRoles::OptionalRole )
+        return m_requirments.at(index.row())->optional();
+
+    return QVariant();
+}
+
+QHash<int, QByteArray> RequirementSourceModel::roleNames() const
+{
+    return QHash<int, QByteArray>{
+        {RequirementRoles::NameRole, "name"},
+        {RequirementRoles::TypeRole, "type"},
+        {RequirementRoles::ValueRole, "value"},
+        {RequirementRoles::OptionalRole, "optional"}
+    };
+}
---
+++ core/Source/Model/RequirementSourceModel.h
@@ -0,0 +1,36 @@
+#ifndef REQUIREMENTSOURCEMODEL_H
+#define REQUIREMENTSOURCEMODEL_H
+
+#include <QAbstractListModel>
+#include <QObject>
+#include <QQmlEngine>
+
+#include "../Common/RequirementSource.h"
+
+class RequirementSourceModel : public QAbstractListModel
+{
+    Q_OBJECT
+    QML_ELEMENT
+
+public:
+    enum RequirementRoles {
+        NameRole = Qt::UserRole+1,
+        TypeRole,
+        ValueRole,
+        OptionalRole
+    };
+
+    explicit RequirementSourceModel(QObject *parent = nullptr);
+    explicit RequirementSourceModel(QVector<RequirementSource*>* requirements, QObject *parent = nullptr);
+
+    int rowCount(const QModelIndex& parent = QModelIndex()) const;
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
+
+protected:
+    QHash<int, QByteArray> roleNames() const;
+
+private:
+    QVector<RequirementSource*> m_requirments;
+};
+
+#endif // REQUIREMENTSOURCEMODEL_H
